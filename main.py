0f4fdac3abafc03eb53a42c685d88e6bbedcaaa2
adii21-Ux <adityasangave2112@gmail.com>
2022-06-08 21:34:59 +0530

    Added books controllers

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import models
 
 from database import Base, engine, SessionLocal
 from sqlalchemy.orm import Session 
+from sqlalchemy import func, desc
 
 Base.metadata.create_all(engine)
 def getSession():
@@ -15,6 +16,54 @@ def getSession():
 
 app = FastAPI()
 
+# book controllers
+@app.get('/issue/book')
+def get(session:Session=Depends(getSession)):
+    issuedBooks = session.query(models.Books).all()
+    return issuedBooks
+
+def updateInventory(session, id, operation):
+    bookObj = session.query(models.Inventory).get(id)
+    if operation=="borrow":
+        bookObj.quantity = bookObj.quantity-1
+        
+        session.commit()
+    else:
+        bookObj.quantity = bookObj.quantity+1
+        
+    session.commit()
+    
+@app.post('/issue/book/{rollNo}/{bookName}')
+def post(rollNo:int, bookName:str,session:Session=Depends(getSession)):
+    bookQuery = session.query(models.Inventory).filter(models.Inventory.bookName==bookName).first()
+    if bookQuery.quantity > 0:
+        studentQuery = session.query(models.Books).filter(models.Books.assignedTO==rollNo).all()
+        if len(studentQuery) < 3:
+            issue = models.Books(bookName=bookName, assignedTO =rollNo, bookId=bookQuery.id)
+            session.add(issue)
+            session.commit()
+            session.refresh(issue)
+            
+            updateInventory(session, bookQuery.id, "borrow")
+        else:
+            return JSONResponse({'Limit Reacehed': 'Student have already borrowed 3 books'})
+    else:
+        return Response("Sorry, The book is not available")
+    return issue
+
+@app.delete('/receive/book/{rollNo}/{bookName}')
+def delete(rollNo:int, bookName:str,session:Session=Depends(getSession)):
+    issuedBook = session.query(models.Books).filter(models.Books.assignedTO==rollNo).filter(models.Books.bookName==bookName).first()
+    if issuedBook:
+        updateInventory(session, issuedBook.id, "receive")
+        
+        session.delete(issuedBook)
+        session.commit()
+    else:
+        return Response("Oops, Some error occured")
+        
+    return issuedBook
+
 # register Student
 @app.post('/add/student/')
 def post(student:schemas.Student, session:Session=Depends(getSession)):
@@ -24,11 +73,16 @@ def post(student:schemas.Student, session:Session=Depends(getSession)):
     session.refresh(student)
     return student
 
+@app.get('/all/students')
+def get(session:Session=Depends(getSession)):
+    students = session.query(models.Student).all()
+    return students
+
 # inventory controllers
 
-@app.get('/get/inventory/{id}')
-def get(id:int, session:Session=Depends(getSession)):
-    book = session.query(models.Inventory).get(id)
+@app.get('/get/inventory/{bookName}')
+def get(bookName:str, session:Session=Depends(getSession)):
+    book = session.query(models.Inventory).filter(models.Inventory.bookName==bookName).first()
     return book
 
 @app.get('/get/inventory')
@@ -45,10 +99,9 @@ def post(book:schemas.Inventory, session: Session = Depends(getSession)):
     
     return book
 
-@app.put('/update/inventory/{id}')
-def put(id:int, book:schemas.Inventory, session: Session = Depends(getSession)):
-    bookObj = session.query(models.Inventory).get(id)
-    bookObj.bookName = book.bookName
+@app.put('/update/inventory/{bookName}')
+def put(bookName:str, book:schemas.updateInventory, session: Session = Depends(getSession)):
+    bookObj = session.query(models.Inventory).filter(models.Inventory.bookName==bookName).first()
     bookObj.quantity = book.quantity
     
     session.commit()
--- schemas.py
+++ schemas.py
@@ -1,16 +1,36 @@
 from pydantic import BaseModel,EmailStr
+from typing import List
 
 class Inventory(BaseModel):
     bookName : str
     quantity : int
     
-class Student(BaseModel):
-    rollNo:int
-    name:str
-    standard:int
-    email:EmailStr
+class updateInventory(BaseModel):
+    quantity : int
 
 class Books(BaseModel):
     bookName:str
     assignedTo:int
     bookId:int
+    
+    class Config:
+        orm_mode = True
+        
+class issueBook(Books):
+    pass
+
+class receiveBook(Books):
+    pass
+        
+class Student(BaseModel):
+    rollNo:int
+    name:str
+    standard:int
+    email:EmailStr
+        
+class BooksList(Books):
+    assignedTo: int
+    students = Student
+    
+    class Config:
+        orm_mode = True
