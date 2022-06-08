1e1a2d17848ca9f8b541a3488d3bfcf0ba946621
adii21-Ux <adityasangave2112@gmail.com>
2022-06-08 19:04:57 +0530

    Created Inventory management controllers

--- main.py
+++ main.py
@@ -23,3 +23,33 @@ def post(student:schemas.Student, session:Session=Depends(getSession)):
     session.commit()
     session.refresh(student)
     return student
+
+# inventory controllers
+
+@app.get('/get/inventory/{id}')
+def get(id:int, session:Session=Depends(getSession)):
+    book = session.query(models.Inventory).get(id)
+    return book
+
+@app.get('/get/inventory')
+def get(session: Session = Depends(getSession)):
+    books = session.query(models.Inventory).all()
+    return books
+    
+@app.post('/add/inventory')
+def post(book:schemas.Inventory, session: Session = Depends(getSession)):
+    book = models.Inventory(bookName=book.bookName, quantity=book.quantity)
+    session.add(book)
+    session.commit()
+    session.refresh(book)
+    
+    return book
+
+@app.put('/update/inventory/{id}')
+def put(id:int, book:schemas.Inventory, session: Session = Depends(getSession)):
+    bookObj = session.query(models.Inventory).get(id)
+    bookObj.bookName = book.bookName
+    bookObj.quantity = book.quantity
+    
+    session.commit()
+    return bookObj
