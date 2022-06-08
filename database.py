b7bc87cdc8bf90acd6db58eea29d42fc38c58541
adii21-Ux <adityasangave2112@gmail.com>
2022-06-08 21:34:20 +0530

    Database changes

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine("sqlite:///library.db")
+engine = create_engine("sqlite:///library.db",connect_args={'check_same_thread': False})
 
 Base = declarative_base()
 
