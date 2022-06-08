37680f550f359fba411c0605fd698158433d0733
adii21-Ux <adityasangave2112@gmail.com>
2022-06-08 17:13:14 +0530

    Database connection objects

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine("sqlite:///library.db")
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
